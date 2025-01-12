205a974e362e3b95ab082d8844fe8c96ed2accb1
sital002 <sitaldhikari002@gmail.com>
2025-01-12 17:32:30 +0545

    feature: add and get student

--- student-grade-management-system/src/Main.java
+++ student-grade-management-system/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         StudentManagement studentManagement = new StudentManagement();
         studentManagement.addS(new Student("Ram","Poudel",35,Gender.MALE,6,20));
-        Student ram = studentManagement.get(20);
+//        Student ram = studentManagement.get(20);
 //        System.out.println(ram.firstName+" "+ram.lastName);
 //        studentManagement.addStudent(new Student("Ram","Poudel",35,Gender.MALE,6,20));
     }
