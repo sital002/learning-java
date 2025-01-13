be8d2d8e0fa0bea7e6ea2b5f4254e0ab165be7e0
sital002 <sitaldhikari002@gmail.com>
2025-01-13 17:01:52 +0545

    test commit

--- student-grade-management-system/src/Main.java
+++ student-grade-management-system/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         studentManagement.add(new Student("Ram","Poudel",35,Gender.MALE,6,20));
         studentManagement.add(new Student("Hari","Chaudhary",30,Gender.MALE,9,21));
         studentManagement.display();
-        Student ram = studentManagement.get(20);
-        System.out.println(ram.firstName+" "+ram.lastName);
+//        Student ram = studentManagement.get(20);
+//        System.out.println(ram.firstName+" "+ram.lastName);
 //        studentManagement.addStudent(new Student("Ram","Poudel",35,Gender.MALE,6,20));
     }
 }
